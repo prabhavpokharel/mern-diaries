5314ba3221151605a3ea17776afff42b20cf353a
Prabhav Pokharel <akacaptainfury@gmail.com>
2026-06-23 22:17:04 +0545

    getting started with js

---
+++ 04-Javascript/myScript.js
@@ -0,0 +1,5 @@
+console.log("Good Afternoon")
+
+let a = 5 // this declared variable will also be applicable and accessed from console of browser. 
+
+// we can also use console of the browser to write some code but the disadvantage is that when refreshed the code will be lost.
